41af571e91f9f53a5f3abbe8277645ae3db4256b
ericagordy <egordy@uw.edu>
2023-07-20 17:48:00 -0700

    jest tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+//example test
+test('checkbox test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  userEvent.click(screen.getByTestId('checkbox'));
+  expect(screen.getByTestId('checkbox')).toBeChecked()
+});
+
+//my test
+test('Enter text test', () => {
+  render(<App />);
+  expect(screen.getByText('Enter')).toBeInTheDocument();
 });
